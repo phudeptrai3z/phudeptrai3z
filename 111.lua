5fdc773bdaa12bdab5ed0f569ef503b07c314752
phudeptrai3z <162773090+phudeptrai3z@users.noreply.github.com>
2024-03-10 17:57:30 +0700

    Update 111.lua

--- 111.lua
+++ 111.lua
@@ -1,1 +1,99 @@
-local Kavo = {} local tween = game:GetService("TweenService") local tweeninfo = TweenInfo.new local input = game:GetService("UserInputService") local run = game:GetService("RunService") local Utility = {} local Objects = {} function Kavo:DraggingEnabled(frame, parent) parent = parent or frame -- stolen from wally or kiriot, kek local dragging = false local dragInput, mousePos, framePos frame.InputBegan:Connect(function(input) if input.UserInputType == Enum.UserInputType.MouseButton1 then dragging = true mousePos = input.Position framePos = parent.Position input.Changed:Connect(function() if input.UserInputState == Enum.UserInputState.End then dragging = false end end) end end) frame.InputChanged:Connect(function(input) if input.UserInputType == Enum.UserInputType.MouseMovement then dragInput = input end end) input.InputChanged:Connect(function(input) if input == dragInput and dragging then local delta = input.Position - mousePos parent.Position = UDim2.new(framePos.X.Scale, framePos.X.Offset + delta.X, framePos.Y.Scale, framePos.Y.Offset + delta.Y) end end) end function Utility:TweenObject(obj, properties, duration, ...) tween:Create(obj, tweeninfo(duration, ...), properties):Play() end local themes = { SchemeColor = Color3.fromRGB(74, 99, 135), Background = Color3.fromRGB(36, 37, 43), Header = Color3.fromRGB(28, 29, 34), TextColor = Color3.fromRGB(255,255,255), ElementColor = Color3.fromRGB(32, 32, 38) } local themeStyles = { DarkTheme = { SchemeColor = Color3.fromRGB(64, 64, 64), Background = Color3.fromRGB(0, 0, 0), Header = Color3.fromRGB(0, 0, 0), TextColor = Color3.fromRGB(255,255,255), ElementColor = Color3.fromRGB(20, 20, 20) }, LightTheme = { SchemeColor = Color3.fromRGB(150, 150, 150), Background = Color3.fromRGB(255,255,255), Header = Color3.fromRGB(200, 200, 200), TextColor = Color3.fromRGB(0,0,0), ElementColor = Color3.fromRGB(224, 224, 224) }, BloodTheme = { SchemeColor = Color3.fromRGB(227, 27, 27), Background = Color3.fromRGB(10, 10, 10), Header = Color3.fromRGB(5, 5, 5), TextColor = Color3.fromRGB(255,255,255), ElementColor = Color3.fromRGB(20, 20, 20) }, GrapeTheme = { SchemeColor = Color3.fromRGB(166, 71, 214), Background = Color3.fromRGB(64, 50, 71), Header = Color3.fromRGB(36, 28, 41), TextColor = Color3.fromRGB(255,255,255), ElementColor = Color3.fromRGB(74, 58, 84) }, Ocean = { SchemeColor = Color3.fromRGB(86, 76, 251), Background = Color3.fromRGB(26, 32, 58), Header = Color3.fromRGB(38, 45, 71), TextColor = Color3.fromRGB(200, 200, 200), ElementColor = Color3.fromRGB(38, 45, 71) }, Midnight = { SchemeColor = Color3.fromRGB(26, 189, 158), Background = Color3.fromRGB(44, 62, 82), Header = Color3.fromRGB(57, 81, 105), TextColor = Color3.fromRGB(255, 255, 255), ElementColor = Color3.fromRGB(52, 74, 95) }, Sentinel = { SchemeColor = Color3.fromRGB(230, 35, 69), Background = Color3.fromRGB(32, 32, 32), Header = Color3.fromRGB(24, 24, 24), TextColor = Color3.fromRGB(119, 209, 138), ElementColor = Color3.fromRGB(24, 24, 24) }, Synapse = { SchemeColor = Color3.fromRGB(46, 48, 43), Background = Color3.fromRGB(13, 15, 12), Header = Color3.fromRGB(36, 38, 35), TextColor = Color3.fromRGB(152, 99, 53), ElementColor = Color3.fromRGB(24, 24, 24) }, Serpent = { SchemeColor = Color3.fromRGB(0, 166, 58), Background = Color3.fromRGB(31, 41, 43), Header = Color3.fromRGB(22, 29, 31), TextColor = Color3.fromRGB(255,255,255), ElementColor = Color3.fromRGB(22, 29, 31) } } local oldTheme = "" local SettingsT = { } local Name = "KavoConfig.JSON" pcall(function() if not pcall(function() readfile(Name) end) then writefile(Name, game:service'HttpService':JSONEncode(SettingsT)) end Settings = game:service'HttpService':JSONEncode(readfile(Name)) end) local LibName = tostring(math.random(1, 100))..tostring(math.random(1,50))..tostring(math.random(1, 100)) function Kavo:ToggleUI() if game.CoreGui[LibName].Enabled then game.CoreGui[LibName].Enabled = false else game.CoreGui[LibName].Enabled = true end end function Kavo.CreateLib(kavName, themeList) if not themeList then themeList = themes end if themeList == "DarkTheme" then themeList = themeStyles.DarkTheme elseif themeList == "LightTheme" then themeList = themeStyles.LightTheme elseif themeList == "BloodTheme" then themeList = themeStyles.BloodTheme elseif themeList == "GrapeTheme" then themeList = themeStyles.GrapeTheme elseif themeList == "Ocean" then themeList = themeStyles.Ocean elseif themeList == "Midnight" then themeList = themeStyles.Midnight elseif themeList == "Sentinel" then themeList = themeStyles.Sentinel elseif themeList == "Synapse" then themeList = themeStyles.Synapse elseif themeList == "Serpent" then themeList = themeStyles.Serpent else if themeList.SchemeColor == nil then themeList.SchemeColor = Color3.fromRGB(74, 99, 135) elseif themeList.Background == nil then themeList.Background = Color3.fromRGB(36, 37, 43) elseif themeList.Header == nil then themeList.Header = Color3.fromRGB(28, 29, 34) elseif themeList.TextColor == nil then themeList.TextCol
+--Triet2804#4818
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local menu1 = Instance.new("Frame")
+local menuname = Instance.new("TextLabel")
+local INY = Instance.new("TextButton")
+local esp = Instance.new("TextButton")
+local exit = Instance.new("TextButton")
+local openmenu = Instance.new("Frame")
+local openmenubutton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+menu1.Name = "menu1"
+menu1.Parent = ScreenGui
+menu1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+menu1.Position = UDim2.new(0.363703161, 0, 0.23950617, 0)
+menu1.Size = UDim2.new(0, 436, 0, 266)
+menu1.Active = true
+menu1.Visible = true
+menu1.Draggable = true
+
+menuname.Name = "menuname"
+menuname.Parent = menu1
+menuname.BackgroundColor3 = Color3.fromRGB(1, 255, 247)
+menuname.Size = UDim2.new(0, 436, 0, 50)
+menuname.Font = Enum.Font.SourceSans
+menuname.Text = "Menu OP"
+menuname.TextColor3 = Color3.fromRGB(0, 0, 0)
+menuname.TextSize = 35.000
+
+INY.Name = "INY"
+INY.Parent = menu1
+INY.BackgroundColor3 = Color3.fromRGB(88, 88, 88)
+INY.Position = UDim2.new(0.0642201826, 0, 0.300751865, 0)
+INY.Size = UDim2.new(0, 144, 0, 50)
+INY.Font = Enum.Font.SourceSans
+INY.Text = "Infiniyield"
+INY.TextColor3 = Color3.fromRGB(0, 0, 0)
+INY.TextSize = 14.000
+INY.MouseButton1Down:Connect(function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source')))()    
+end)
+
+esp.Name = "esp"
+esp.Parent = menu1
+esp.BackgroundColor3 = Color3.fromRGB(88, 88, 88)
+esp.Position = UDim2.new(0.582568824, 0, 0.300751865, 0)
+esp.Size = UDim2.new(0, 144, 0, 50)
+esp.Font = Enum.Font.SourceSans
+esp.Text = "ESP"
+esp.TextColor3 = Color3.fromRGB(0, 0, 0)
+esp.TextSize = 14.000
+esp.MouseButton1Down:Connect(function()
+        print("ESP ON")
+end)
+
+exit.Name = "exit"
+exit.Parent = menu1
+exit.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+exit.Position = UDim2.new(0.912844062, 0, 0, 0)
+exit.Size = UDim2.new(0, 38, 0, 30)
+exit.Font = Enum.Font.SourceSans
+exit.Text = "X"
+exit.TextColor3 = Color3.fromRGB(0, 0, 0)
+exit.TextSize = 14.000
+exit.MouseButton1Down:Connect(function()
+        menu1.Visible = false
+        openmenu.Visible = true
+end)
+
+openmenu.Name = "openmenu"
+openmenu.Parent = ScreenGui
+openmenu.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+openmenu.Position = UDim2.new(0, 0, 0.383950621, 0)
+openmenu.Size = UDim2.new(0, 145, 0, 66)
+openmenu.Active = true
+openmenu.Draggable = true
+openmenu.Visible = false
+
+openmenubutton.Name = "openmenubutton"
+openmenubutton.Parent = openmenu
+openmenubutton.BackgroundColor3 = Color3.fromRGB(255, 234, 2)
+openmenubutton.Size = UDim2.new(0, 145, 0, 66)
+openmenubutton.Font = Enum.Font.SourceSans
+openmenubutton.Text = "Open Menu"
+openmenubutton.TextColor3 = Color3.fromRGB(0, 0, 0)
+openmenubutton.TextSize = 27.000
+openmenubutton.MouseButton1Down:Connect(function()
+        menu1.Visible = true
+        openmenu.Visible = false
+end)
