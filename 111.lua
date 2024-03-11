5997acacdced58a81a914376ab53a58d1214f336
phudeptrai3z <162773090+phudeptrai3z@users.noreply.github.com>
2024-03-11 12:36:13 +0700

    Update 111.lua

--- 111.lua
+++ 111.lua
@@ -1,5 +1,3 @@
----// Join MTien Hub Sever To Update New Script \\--
---// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
 _G.Name = "test" -- Put You Name Script Here
 _G.Logo = "16709282906" -- Enter Your ID Image Here
 loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
