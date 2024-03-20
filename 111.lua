e837feac37ee32bc7bed08517a8bd0533ad72724
phudeptrai3z <162773090+phudeptrai3z@users.noreply.github.com>
2024-03-20 12:10:51 +0700

    Update 111.lua
    Ok

--- 111.lua
+++ 111.lua
@@ -1,3 +1,24 @@
-_G.Name = "test" -- Put You Name Script Here
-_G.Logo = "16709282906" -- Enter Your ID Image Here
-loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
+                toFixedFix = function (n, prec) {
+                    var k = Math.pow(10, prec);
+                    return '' + Math.round(n * k) / k;
+                };
+            // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+            s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+            if (s[0].length > 3) {
+                s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+            }
+            if ((s[1] || '').length < prec) {
+                s[1] = s[1] || '';
+                s[1] += new Array(prec - s[1].length + 1).join('0');
+            }
+            return s.join(dec);
+        }
+
+</script>
+
+
+
+<SCRIPT src="/assets/Style/js.js" async=""></SCRIPT>
+</BODY>
+
+</HTML>
